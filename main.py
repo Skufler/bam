b351b23df3097ceef839c6ff5a2f8b32b86c9174
Skufler <matviei.skufin@gmail.com>
2020-05-11 19:11:23 +0300

    v4

--- main.py
+++ main.py
@@ -1,182 +1,255 @@
-import itertools
-
 import cv2
+import matplotlib.pyplot as plt
+import multiprocessing
 import numpy as np
 import pandas as pd
-# from neupy.algorithms import DiscreteBAM
-from neupy.algorithms.memory.base import DiscreteMemory
-from neupy.utils import format_data
-from numpy.core.umath_tests import inner1d
-from scipy.spatial import distance
+import tensorflow as tf
+
+# Set random seed (for reproducibility)
 from tqdm import tqdm
+
+
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+
+np.random.seed(1000)
+tf.set_random_seed(1000)
+
+width = 32
+height = 32
+batch_size = 10
+nb_epochs = 500
+code_length = 1024
+use_gpu = True
 
 IMAGE_WIDTH = 32
 IMAGE_HEIGHT = 32
-
 PATH_PREFIX_1 = r'C:\Users\Skufler\PycharmProjects\comnist\data\letters'
 PATH_PREFIX_2 = r'C:\Users\Skufler\PycharmProjects\comnist\data\letters2'
 PATH_PREFIX_3 = r'C:\Users\Skufler\PycharmProjects\comnist\data\letters3'
 
-# абвгдеёжзийклмнопрстуфхцчшщъыьэюя
-russian_alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-
-
-def bin2sign(matrix):
-    return np.where(matrix == 0, -1, 1)
-
-
-def sign2bin(matrix):
-    return np.where(matrix > 0, 1, 0).astype(int)
-
-
-def hopfield_energy(weight, X, y):
-    return -0.5 * inner1d(X.dot(weight), y)
-
-
-class DiscreteBAM(DiscreteMemory):
-
-    def predict_input(self, y_bin):
-        if self.weight is None:
-            raise Exception('Network not trained')
-
-        self.discrete_validation(y_bin)
-
-        y_bin = format_data(y_bin, is_feature1d=False)
-        y_sign = bin2sign(y_bin)
-        X_sign = np.sign(y_sign.dot(self.weight.T))
-
-        if self.mode == 'sync':
-            return sign2bin(X_sign), y_bin
-
-    def predict_output(self, X_bin):
-        if self.weight is None:
-            raise Exception("Network hasn't been trained yet")
-
-        self.discrete_validation(X_bin)
-
-        X_bin = format_data(X_bin, is_feature1d=False)
-        X_sign = bin2sign(X_bin)
-        y_sign = np.sign(X_sign.dot(self.weight))
-
-        if self.mode == 'sync':
-            return X_bin, sign2bin(y_sign)
-
-    def predict(self, X_bin):
-        return self.predict_output(X_bin)
-
-    def train(self, X_bin, y_bin):
-        self.discrete_validation(X_bin)
-        self.discrete_validation(y_bin)
-
-        X_sign = bin2sign(format_data(X_bin, is_feature1d=False))
-        y_sign = bin2sign(format_data(y_bin, is_feature1d=False))
-
-        _, weight_nrows = X_sign.shape
-        _, weight_ncols = y_sign.shape
-        weight_shape = (weight_nrows, weight_ncols)
-
-        if self.weight is None:
-            self.weight = np.zeros(weight_shape)
-
-        if self.weight.shape != weight_shape:
-            raise ValueError(
-                "Invalid input shapes. Number of input "
-                "features must be equal to {} and {} output "
-                "features".format(weight_nrows, weight_ncols))
-
-        self.weight += X_sign.T.dot(y_sign)
-
-    def energy(self, X_bin, y_bin):
-        self.discrete_validation(X_bin)
-        self.discrete_validation(y_bin)
-
-        X_sign, y_sign = bin2sign(X_bin), bin2sign(y_bin)
-        X_sign = format_data(X_sign, is_feature1d=False)
-        y_sign = format_data(y_sign, is_feature1d=False)
-        nrows, n_features = X_sign.shape
-
-        if nrows == 1:
-            return hopfield_energy(self.weight, X_sign, y_sign)
-
-        output = np.zeros(nrows)
-        for i, rows in enumerate(zip(X_sign, y_sign)):
-            output[i] = hopfield_energy(self.weight, *rows)
-
-        return output
-
 
 def open_image(image_path) -> np.array:
-    _image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    _image = cv2.imread(image_path, cv2.COLOR_BGR2RGB)
     _image = cv2.resize(_image, (IMAGE_HEIGHT, IMAGE_WIDTH))
 
-    (thresh, im_bw) = cv2.threshold(_image, 0, 1, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
-
-    return np.array(im_bw, dtype='int16')
+    return np.array(_image, dtype='int16')
 
 
-def convert_to_bipolar(_image) -> np.array:
-    return np.array([-1 if pixel == 0 else 1 for pixel in _image], dtype='int16')
-
-
-def compute_distances(target, associations):
-    distances = distance.cdist(
-        target,
-        np.array(
-            [[1 if x == 1 else 0 for x in y] for y in associations], dtype='int16'
-        ),
-        "cosine"
-    )[0]
-    min_index = np.argmin(distances)
-    min_distance = distances[min_index]
-    max_similarity = 1 - min_distance
-
-    return max_similarity, russian_alphabet[min_index]
-
-
-def make_pair(input_matrix):
-    return convert_to_bipolar(input_matrix)
-
-
+# Load the dataset
 dataset = pd.read_csv(r'C:\Users\Skufler\PycharmProjects\comnist\data\letters3.csv')
 dataset.head()
 
 images_path = dataset['file']
 
-a_i = np.zeros(shape=(0, 1024), dtype='int16')
-b_i = np.zeros(shape=(0, 1024), dtype='int16')
-COUNT = 2
+a_i = np.zeros(shape=(0, 32, 32, 3), dtype='int16')
+b_i = np.zeros(shape=(0, 32, 32, 3), dtype='int16')
+COUNT = 50
 
 for index, path in tqdm(enumerate(images_path), total=len(images_path)):
-    if path == '24_34.png':
-        continue
     if int(path[3:6]) > 230 + COUNT:
         continue
+
     image = open_image(PATH_PREFIX_3 + '\\' + path)
 
     if path[3:6:] == '231':
         for i in range(COUNT - 1):
-            b_i = np.vstack((
+            b_i = np.insert(
                 b_i,
-                np.array(image.ravel())
-            ))
+                b_i.shape[0],
+                np.array(image),
+                axis=0
+            )
     else:
-        a_i = np.vstack((
+        a_i = np.insert(
             a_i,
-            image.ravel()
-        ))
-a_i = np.delete(a_i, 0, axis=0)
-b_i = np.delete(b_i, 0, axis=0)
+            a_i.shape[0],
+            image,
+            axis=0
+        )
 
-bam = DiscreteBAM(mode='sync')
-bam.train(a_i, b_i)
+np.delete(a_i, 0, axis=0)
+np.delete(b_i, 0, axis=0)
+
+# Select 50 samples
+X_train = a_i
+X_source = X_train[0:COUNT * 33]
+X_dest = X_source.copy()
+np.random.shuffle(X_dest)
 
 
-dataset = pd.read_csv(r'C:\Users\Skufler\PycharmProjects\comnist\data\letters.csv')
-images_path = dataset['file']
+def encoder(encoder_input):
+    # Convolutional layer 1
+    conv1 = tf.layers.conv2d(inputs=encoder_input,
+                             filters=32,
+                             kernel_size=(3, 3),
+                             kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                             activation=tf.nn.tanh)
 
-x = 0
-for path in tqdm(images_path, total=len(images_path)):
-    if russian_alphabet[int(path[:2]) - 1] == compute_distances(bam.predict(open_image(PATH_PREFIX_1 + '\\' + path).ravel())[0], np.unique(b_i, axis=0))[1]:
-        x += 1
+    # Convolutional output (flattened)
+    conv_output = tf.contrib.layers.flatten(conv1)
 
-print(x / len(images_path))
+    # Encoder Dense layer 1
+    d_layer_1 = tf.layers.dense(inputs=conv_output,
+                                units=1024,
+                                activation=tf.nn.tanh)
+
+    # Code layer
+    code_layer = tf.layers.dense(inputs=d_layer_1,
+                                 units=code_length,
+                                 activation=tf.nn.tanh)
+
+    return code_layer
+
+
+def decoder(code_sequence, bs):
+    # Decoder Dense layer 1
+    d_layer_1 = tf.layers.dense(inputs=code_sequence,
+                                units=1024,
+                                activation=tf.nn.tanh)
+
+    # Code output layer
+    code_output = tf.layers.dense(inputs=d_layer_1,
+                                  units=(height - 2) * (width - 2) * 3,
+                                  activation=tf.nn.tanh)
+
+    # Deconvolution input
+    deconv_input = tf.reshape(code_output, (bs, height - 2, width - 2, 3))
+
+    # Deconvolution layer 1
+    deconv1 = tf.layers.conv2d_transpose(inputs=deconv_input,
+                                         filters=3,
+                                         kernel_size=(3, 3),
+                                         kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                                         activation=tf.sigmoid)
+
+    # Output batch
+    output_batch = tf.cast(tf.reshape(deconv1, (bs, height, width, 3)) * 255.0, tf.uint8)
+
+    return deconv1, output_batch
+
+
+def create_batch(t):
+    X = np.zeros((batch_size, height, width, 3), dtype=np.float32)
+    Y = np.zeros((batch_size, height, width, 3), dtype=np.float32)
+
+    if t < X_source.shape[0] - batch_size:
+        tmax = t + batch_size
+    else:
+        tmax = X_source.shape[0]
+
+    for k, image in enumerate(X_source[t:tmax]):
+        X[k, :, :, :] = image / 255.0
+
+    for k, image in enumerate(X_dest[t:tmax]):
+        Y[k, :, :, :] = image / 255.0
+
+    return X, Y
+
+
+# Create a Tensorflow Graph
+graph = tf.Graph()
+
+with graph.as_default():
+    with tf.device('/cpu:0'):
+        # Global step
+        global_step = tf.Variable(0, trainable=False)
+
+    with tf.device('/gpu:0' if use_gpu else '/cpu:0'):
+        # Input batch
+        input_images = tf.placeholder(tf.float32, shape=(None, height, width, 3))
+
+        # Output batch
+        output_images = tf.placeholder(tf.float32, shape=(None, height, width, 3))
+
+        # Batch_size
+        t_batch_size = tf.placeholder(tf.int32, shape=())
+
+        # Encoder
+        code_layer = encoder(encoder_input=input_images)
+
+        # Decoder
+        deconv_output, output_batch = decoder(code_sequence=code_layer,
+                                              bs=t_batch_size)
+
+        # Reconstruction L2 loss
+        loss = tf.nn.l2_loss(output_images - deconv_output)
+
+        # Training operations
+        learning_rate = tf.train.exponential_decay(learning_rate=0.00025,
+                                                   global_step=global_step,
+                                                   decay_steps=int(X_source.shape[0] / (2 * batch_size)),
+                                                   decay_rate=0.9,
+                                                   staircase=True)
+
+        trainer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+
+        training_step = trainer.minimize(loss)
+
+
+def predict(X, bs=1):
+    feed_dict = {
+        input_images: X.reshape((1, height, width, 3)) / 255.0,
+        output_images: np.zeros((bs, height, width, 3), dtype=np.float32),
+        t_batch_size: bs
+    }
+
+    return session.run([output_batch], feed_dict=feed_dict)[0]
+
+
+def story(t):
+    oimages = np.zeros(shape=(20, height, width, 3), dtype=np.uint8)
+    oimages[0, :, :, :] = X_source[t]
+
+    for i in range(1, 20):
+        oimages[i, :, :, :] = predict(oimages[i - 1])
+
+    fig, ax = plt.subplots(2, 10, figsize=(18, 4))
+
+    for i in range(2):
+        for j in range(10):
+            ax[i, j].get_xaxis().set_visible(False)
+            ax[i, j].get_yaxis().set_visible(False)
+            ax[i, j].imshow(oimages[(10 * i) + j])
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    # Create a Tensorflow Session
+    config = tf.ConfigProto(intra_op_parallelism_threads=multiprocessing.cpu_count(),
+                            inter_op_parallelism_threads=multiprocessing.cpu_count(),
+                            allow_soft_placement=True,
+                            device_count={'CPU': 1,
+                                          'GPU': 1 if use_gpu else 0})
+
+    session = tf.InteractiveSession(graph=graph, config=config)
+
+    # Initialize all variables
+    tf.global_variables_initializer().run()
+
+    # Train the model
+    for e in range(nb_epochs):
+        total_loss = 0.0
+
+        for t in range(0, X_source.shape[0], batch_size):
+            X, Y = create_batch(t)
+
+            feed_dict = {
+                input_images: X,
+                output_images: Y,
+                t_batch_size: batch_size
+            }
+
+            _, t_loss = session.run([training_step, loss], feed_dict=feed_dict)
+            total_loss += t_loss
+
+        print('Epoch {} - Loss: {}'.
+              format(e + 1,
+                     total_loss / float(X_train.shape[0])))
+
+    #print(predict(open_image(PATH_PREFIX_1 + '\\' + '24_34.png')))
+
+    story(0)
+    story(1)
+    story(5)
+    story(9)
